aeaeaab6beed520e2541bc5683511f5f2f6fbd41
Fabiano Lau Dutra <fabianolaudutra@gmail.com>
2025-03-17 19:02:49 -0300

    Add project into repository

---
+++ backend/src/modules/students/students-controller.js
@@ -0,0 +1,38 @@
+const asyncHandler = require("express-async-handler");
+const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
+
+const handleGetAllStudents = asyncHandler(async (req, res) => {
+    //write your code
+    const students = await getAllStudents();
+    res.json(students); 
+    
+
+});
+
+const handleAddStudent = asyncHandler(async (req, res) => {
+    //write your code
+
+});
+
+const handleUpdateStudent = asyncHandler(async (req, res) => {
+    //write your code
+
+});
+
+const handleGetStudentDetail = asyncHandler(async (req, res) => {
+    //write your code
+
+});
+
+const handleStudentStatus = asyncHandler(async (req, res) => {
+    //write your code
+
+});
+
+module.exports = {
+    handleGetAllStudents,
+    handleGetStudentDetail,
+    handleAddStudent,
+    handleStudentStatus,
+    handleUpdateStudent,
+};
