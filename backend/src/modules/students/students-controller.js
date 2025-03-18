07ee451145aa3377e982c67b749a49f732312a40
Fabiano Lau Dutra <fabianolaudutra@gmail.com>
2025-03-17 21:55:50 -0300

    backend: Added operation student missing in the crud

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -1,32 +1,37 @@
 const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
-const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
+const handleGetAllStudents = asyncHandler(async (req, res) => {    
     const students = await getAllStudents();
-    res.json(students); 
-    
-
+    res.json({ students }); 
 });
 
-const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
+const handleAddStudent = asyncHandler(async (req, res) => {    
+    const { payload } = req.body;
+    const message = await addNewStudent({ payload });
+    res.json(message);
 
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const { payload} = req.body;
+    const message = await updateStudent({ payload});
+    res.json(message);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
+    const { id } = req.params;
+    const student = await getStudentDetail(id);
+    res.json(student);
 
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const { status } = req.body;
+    const message = await setStudentStatus({ id, status });
+    res.json(message);
 });
 
 module.exports = {
